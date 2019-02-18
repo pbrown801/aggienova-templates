521f0a3b131bbcb1c3bbcdf1f15bb055c70df12f
Tate Walker <tatewalker@comcast.net>
2019-02-18 11:53:24 -0600

    added conversion from dat to csv

---
+++ python/dat_to_csv.py
@@ -0,0 +1,8 @@
+#converter script between .dat to .csv
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('../input/vega.dat',skiprows=1,header=None,skipinitialspace=True,sep=' ',names=['Wavelength','Flux'])
+out_df = pd.DataFrame({'Epoch':[0]*len(df/2)})
+out_df = out_df.join(df)
+out_df.to_csv('../output/dat_to_csv.csv')
