0816e6e1d56da234c7fa91615898ca60b5f3b012
Tate Walker <tatewalker@comcast.net>
2019-02-20 15:04:51 -0600

    updated utilities and pipeline

--- python/mangle_simple.py
+++ python/mangle_simple.py
@@ -2,16 +2,16 @@
 
 import pandas as pd
 import numpy as np
-from ../old/speccounts import *
+from speccounts import *
 
 
 def pivot_wavelength(Filter):
-    
+
     filter_wave,filter_tp = np.loadtxt(Filter, dtype = float, usecols=(0,1), unpack=True)
 
     numerator = np.trapz(filter_tp*filter_wave,filter_wave)
     denominator = np.trapz(filter_tp/filter_wave,filter_wave)
-    
+
     pivot_lambda = np.sqrt(numerator/denominator)
 
     return pivot_lambda
@@ -20,25 +20,25 @@ def pivot_wavelength(Filter):
 
 def mangle_simple(templatespectrum,filtercurves_list, counts_in):
 
-input_wave,input_flux = np.loadtxt(templatespectrum, dtype=float,usecols=(0,1),unpack=True)
+    input_wave,input_flux = np.loadtxt(templatespectrum, dtype=float,usecols=(0,1),unpack=True)
 
 
-for l in range(len(filtercurves_list)):
-	pivot_array.append(pivot_wavelength(Filters[l]))
+    for l in range(len(filtercurves_list)):
+    	pivot_array.append(pivot_wavelength(Filters[l]))
 
 
 
-#######  This comes as previous code (under old/ for just the UVOT filters)
-input_wave,input_flux = np.loadtxt('spectra/Gaia16apd_uv.dat', dtype=float,usecols=(0,1),unpack=True)
+    #######  This comes as previous code (under old/ for just the UVOT filters)
+    input_wave,input_flux = np.loadtxt('spectra/Gaia16apd_uv.dat', dtype=float,usecols=(0,1),unpack=True)
 
-counts_array=specin_countsout(input_wave, input_flux)
-print(counts_array)
+    counts_array=specin_countsout(input_wave, input_flux)
+    print(counts_array)
 
-ratio=counts_in/counts_array
+    ratio=counts_in/counts_array
 
-manglefunction= np.interp(input_wave, pivot_array, ratio)
+    manglefunction= np.interp(input_wave, pivot_array, ratio)
 
-mangledspectrumflux=input_flux*ratio
+    mangledspectrumflux=input_flux*ratio
 
 
-return input_wave, mangledspectrumflux
+    return input_wave, mangledspectrumflux
--- python/run_pipeline.py
+++ python/run_pipeline.py
@@ -1,43 +1,41 @@
 #  take an input spectrum
 import pandas as pd
 import numpy as np
+from utilities import *
+import mangle_simple
 
 
 ####### this would come from the data input side
-#######  output is a list of filters and an array of count rates 
-#######  with the number of rows equal to the number of epochs 
+#######  output is a list of filters and an array of count rates
+#######  with the number of rows equal to the number of epochs
 #######  and the number of columns equal to the number of filters.
 #######  If that is output as a csv file, then this needs to read that in
 #######  or call a file to do so.
 
-filtercurves_list = ['UVW2_2010','UVM2_2010','UVW1_2010','U_UVOT','B_UVOT','V_UVOT'] ### STRING LIST
-filtercurves_list = "../filters/" + filtercurves_list + ".dat"
-print(filtercurves_list)
+filter_curves_list = ['UVW2_2010', 'UVM2_2010', 'UVW1_2010', 'U_UVOT', 'B_UVOT', 'V_UVOT'] ### STRING LIST
+for i in range(0,len(filter_curves_list)):
+    filter_curves_list[i] = "../filters/" + filter_curves_list[i] + ".txt"
 
-
-
-#######  This would come from a code for reading in each filter from the list and outputting the effective wavelength in each filter
-
-    filtereffwavelengths=[2030,2231,2634,3501,4329,5402] 
 
 ####### read in the first row of count rates
-singleepochtestmagcounts=[99.2572, 40.0928, 391.629, 3538.04, 4730.92, 1505.18]  #  these come from SN2011fe
+# total_counts #this should return an array of count rates
+single_epoch_test_mag_counts = [99.2572, 40.0928, 391.629, 3538.04, 4730.92, 1505.18]  #  these come from SN2011fe
 
-epoch=3.4
+epoch = 3.4
 
 #######  Assign a template spectrum to use
 
-templatespectrum="../spectra/SN2017erp_hst_20170629.dat"
+templatespectrum = "../spectra/SN2017erp_hst_20170629.dat"
 
 #######  mangle the spectrum to match the given countrates
 
-counts_in=singleepochtestmagcounts
-mangledspecwave,mangledspecflux= specfilterscountsin_mangledspecout(templatespectrum,filtercurves_list, counts_in)
+counts_in = single_epoch_test_mag_counts
+mangled_spec_wave, mangled_spec_flux = mangle_simple(template_spectrum, filter_curves_list, counts_in)
 
+# mangling outputs spectrum that matches input counts by multiplying func by og spec
 
-
-####### write a 3 column csv file with the epoch (constant for each spectrum), mangledspecwave, mangledspecflux for each row of mangledspecwave,mangledspecflux
-
+####### write a 3 column csv file with the epoch (constant for each spectrum), mangledspecwave, mangledspecflux for each row of mangledspecwave,mangledspecflux ***TATE WROTE THIS ALREADY***
+spectrum_to_csv(mangled_spec_wave,mangled_spec_flux)
 
 
 #######  Now read in that file
