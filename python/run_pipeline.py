a7436107de054ced55e487be0279b9b879a1649d
Tate Walker <tatewalker@comcast.net>
2019-02-20 16:22:56 -0600

    pipeline update

--- python/run_pipeline.py
+++ python/run_pipeline.py
@@ -12,6 +12,9 @@ import mangle_simple
 #######  If that is output as a csv file, then this needs to read that in
 #######  or call a file to do so.
 
+
+#assign sn name at beginning and look for that file as an input
+
 filter_curves_list = ['UVW2_2010', 'UVM2_2010', 'UVW1_2010', 'U_UVOT', 'B_UVOT', 'V_UVOT'] ### STRING LIST
 for i in range(0,len(filter_curves_list)):
     filter_curves_list[i] = "../filters/" + filter_curves_list[i] + ".txt"
@@ -19,9 +22,9 @@ for i in range(0,len(filter_curves_list)):
 
 ####### read in the first row of count rates
 # total_counts #this should return an array of count rates
-single_epoch_test_mag_counts = [99.2572, 40.0928, 391.629, 3538.04, 4730.92, 1505.18]  #  these come from SN2011fe
+single_epoch_test_mag_counts = [99.2572, 40.0928, 391.629, 3538.04, 4730.92, 1505.18]  #  these come from SN2011fe #THESE WILL COME FROM SNtest
 
-epoch = 3.4
+epoch = 3.4 # future : epoch = mjd - 0
 
 #######  Assign a template spectrum to use
 
@@ -35,7 +38,7 @@ mangled_spec_wave, mangled_spec_flux = mangle_simple(template_spectrum, filter_c
 # mangling outputs spectrum that matches input counts by multiplying func by og spec
 
 ####### write a 3 column csv file with the epoch (constant for each spectrum), mangledspecwave, mangledspecflux for each row of mangledspecwave,mangledspecflux ***TATE WROTE THIS ALREADY***
-spectrum_to_csv(mangled_spec_wave,mangled_spec_flux)
+spectrum_to_csv(mangled_spec_wave,mangled_spec_flux) #ouput name = SNtest_spectral_series.csv
 
 
 #######  Now read in that file
