76770582c7f19800c1e16b79a427df1757048cbc
Tate Walker <tatewalker@comcast.net>
2019-03-06 00:22:00 -0600

    updated pipeline to passto plotting

--- python/run_pipeline.py
+++ python/run_pipeline.py
@@ -3,57 +3,35 @@ import pandas as pd
 import numpy as np
 from utilities import *
 from mangle_simple import *
+from validation_plotting import *
+
+if __name__ = "__main__":
+    sn_name = input('Supernova name: ') #assign sn name at beginning and look for that file as an input
+    inFile = '../input/'+'SNtest_countsarray'+'.csv' #make this generic eventually with sn_name
+    mjd_list = []
+    counts_list = []
+
+    with open(inFile) as csvinp:
+        reader = csv.reader(csvinp,delimiter = ',')
+        filter_curves_list = next(reader)[1:]
+        filter_curves_list = list(map('../filters/{0}'.format, filter_curves_list))
+        for row in reader:
+            epoch = float(row[0])-0
+            counts_in = list(map(float,row[1:]))
+
+            mjd_list.append(epoch)
+            counts_list.append(counts_in)
+
+            template_spectrum = "SN2017erp_hst_20170629.dat" #Assign a template spectrum to use
+
+            mangled_spec_wave, mangled_spec_flux = mangle_simple(template_spectrum, filter_curves_list, counts_in) #mangle the spectrum to match the given count rates
+
+            # mangling outputs spectrum that matches input counts by multiplying func by og spec
+
+            spectrum_to_csv(epoch,mangled_spec_wave,mangled_spec_flux,sn_name) #write csv with epoch, mangled_spec_wave, and mangled_spec_flux
 
 
-####### this would come from the data input side
-#######  output is a list of filters and an array of count rates
-#######  with the number of rows equal to the number of epochs
-#######  and the number of columns equal to the number of filters.
-#######  If that is output as a csv file, then this needs to read that in
-#######  or call a file to do so.
+    mjd_list = np.array(mjd_list,dtype='float')
+    counts_list = np.array(counts_list,dtype='float')
 
-
-#assign sn name at beginning and look for that file as an input
-sn_name = input('Supernova name: ')
-inFile = '../input/'+'SNtest_countsarray'+'.csv' #make this generic eventually with sn_name
-mjd_list = np.empty()
-counts_list = np.empty()
-
-with open(inFile) as csvinp:
-    reader = csv.reader(csvinp,delimiter = ',')
-    filter_curves_list = next(reader)[1:]
-    filter_curves_list = list(map('../filters/{0}'.format, filter_curves_list))
-    for row in reader:
-        epoch = float(row[0])-0
-        counts_in = list(map(float,row[1:]))
-
-        mjd_list.vstack(epoch)
-        counts_list.vstack(counts_in)
-
-        template_spectrum = "SN2017erp_hst_20170629.dat" #Assign a template spectrum to use
-
-####### read in the first row of count rates
-
-# total_counts #this should return an array of count rates
-# counts_in = [99.2572, 40.0928, 391.629, 3538.04, 4730.92, 1505.18]  #  these come from SN2011fe #THESE WILL COME FROM SNtest
-
-
-
-#######  mangle the spectrum to match the given countrates
-
-        mangled_spec_wave, mangled_spec_flux = mangle_simple(template_spectrum, filter_curves_list, counts_in)
-
-# mangling outputs spectrum that matches input counts by multiplying func by og spec
-
-####### write a 3 column csv file with the epoch (constant for each spectrum), mangledspecwave, mangledspecflux for each row of mangledspecwave,mangledspecflux ***TATE WROTE THIS ALREADY***
-        spectrum_to_csv(epoch,mangled_spec_wave,mangled_spec_flux,sn_name) #ouput name = SNtest_spectral_series.csv
-
-    print(mjd_list)
-    validation_plotting(filter_curves_list,counts_in)
-
-#######  Now read in that file
-#######  and for each epoch, read in the spectrum and compute count rates in each filter and store in an array
-
-
-
-######  Plot the mangled template count rates and the input count rates on the same plot with MJD or epoch on the x-axis
+    validation_plotting(mjd_list,counts_list) #Plot the mangled template count rates and the input count rates on the same plot with MJD or epoch on the x-axis
