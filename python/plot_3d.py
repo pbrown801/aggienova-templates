ceae1ae77f7841038402015a245be3af6bd5ceb0
Tate Walker <tatewalker@comcast.net>
2019-04-01 09:30:42 -0500

    updated pipeline to prep for 3d

--- python/plot_3d.py
+++ python/plot_3d.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import csv
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -19,10 +21,11 @@ plt.show()
 #Plots file with filename in 3 dimensions: epoch, wavelength, and flux
 #Assumes first row is 'Epoch, Wavelength, Flux' and following rows correspond to those values
 #Does not currently validate file existence
-def plot_3D(df,name):
-    # surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       # linewidth=0, antialiased=False)
-    ax.plot(df.Epoch,df.Wavelength,df.Flux)
+def plot_3D(x,y,z,name):
+    X,Y = np.meshgrid(x,y)
+    surf = ax.plot_surface(X, Y, z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+    # ax.plot(x,y,z)
     plt.title(name)
     ax.set_xlabel('Time (mjd)')
     ax.set_ylabel('Wavelength')
--- python/run_pipeline.py
+++ python/run_pipeline.py
@@ -8,48 +8,55 @@ from validation_plotting import *
 
 if __name__ == "__main__":
     sn_name = input('Supernova name: ') #assign sn name at beginning and look for that file as an input
-    inFile = '../input/'+sn_name+'_countsarray'+'.csv' #make this generic eventually with sn_name
-    mjd_list = []
-    counts_list = []
+    inFile = '../input/'+sn_name+'_countsarray'+'.csv'
 
-    with open(inFile) as csvinp:
-        reader = csv.reader(csvinp,delimiter = ',')
-        filter_curves_list_no_format = next(reader)[1:]
-        filter_curves_list = list(map('../filters/{0}'.format, filter_curves_list_no_format))
-        df = pd.DataFrame(columns= ['Epoch','Wavelength', 'Flux'])
-        for row in reader:
-            epoch = float(row[0])-0
-            counts_in = list(map(float,row[1:]))
+    file = open(inFile).readlines()
+    reader = csv.reader(file,delimiter = ',')
+    filter_curves_list_no_format = next(reader)[1:]
+    filter_curves_list = list(map('../filters/{0}'.format, filter_curves_list_no_format))
+    template_spectrum = "SN2017erp_hst_20170629.dat" #Assign a template spectrum to use
+    row_count = sum(1 for row in file)
+    filter_count = len(filter_curves_list)
+    mjd_list = np.empty((row_count-1))
+    flux_matrix = np.empty((1,row_count-1))
+    flux_matrix.fill(np.nan)
+    wavelengths = np.empty(1)
+    counts_list = np.empty((row_count,filter_count))
+    ind = 0
+    for row in reader:
+        epoch = np.float64(row[0])-0
+        counts_in = np.array(list(map(np.float64,row[1:]))) #theres gotta be an easier way to do this
+        mjd_list[ind]=epoch
+        print(mjd_list[ind])
+        counts_list[ind,:] = counts_in
 
-            mjd_list.append(epoch)
-            counts_list.append(counts_in)
+        mangled_spec_wave, mangled_spec_flux = mangle_simple(template_spectrum, filter_curves_list, counts_in) #mangle the spectrum to match the given count rates
+        if ind == 0:
+            wavelengths =mangled_spec_wave
+            flux_matrix = np.resize(flux_matrix,(len(mangled_spec_flux),row_count-1))
+        flux_matrix[:,ind] = mangled_spec_flux
+        # mangling outputs spectrum that matches input counts by multiplying func by og spec
 
-            template_spectrum = "SN2017erp_hst_20170629.dat" #Assign a template spectrum to use
+        ind+=1
 
-            mangled_spec_wave, mangled_spec_flux = mangle_simple(template_spectrum, filter_curves_list, counts_in) #mangle the spectrum to match the given count rates
-
-            # mangling outputs spectrum that matches input counts by multiplying func by og spec
-
-            data = pd.DataFrame({'Epoch': [epoch]*len(mangled_spec_wave),
-                                 'Wavelength': mangled_spec_wave,
-                                 'Flux': mangled_spec_flux})
-            df = df.append(data)
-
+    df= pd.DataFrame(index = wavelengths,data = flux_matrix,columns= mjd_list)
 
     output_file = '../output/'+sn_name+'_template.csv'
-    df.to_csv(output_file,index=False) #comment this out if you don't want to save everytime FUTURE: make a flag that handles this
+    df.to_csv(output_file,index=True,float_format='%g') #comment this out if you don't want to save everytime FUTURE: make a flag that handles this
 
 
-    mjd_list = np.array(mjd_list,dtype='float')
     counts_list = np.array(counts_list,dtype='float')
     filter_curves_list_no_format = [x.split('_')[0] for x in filter_curves_list_no_format]
 
-    filtered_df = df[(df.Wavelength > 1000) & (df.Wavelength < 10000) & (df.Epoch < 54330)]
-    output_3d = '../output/'+sn_name+'_3d.csv'
-    filtered_df.to_csv(output_3d,index=False) #comment this if you already have your df or dont want to save a filtered version FUTURE: flag to do this
+    # filtered_df = df[(df.Wavelength > 1000) & (df.Wavelength < 10000) & (df.Epoch < 54330)]
+    # output_3d = '../output/'+sn_name+'_3d.csv'
+    # filtered_df.to_csv(output_3d,index=False) #comment this if you already have your df or dont want to save a filtered version FUTURE: flag to do this
 
     validation_plotting(filter_curves_list_no_format,counts_list,mjd_list) #Plot the mangled template count rates and the input count rates on the same plot with MJD or epoch on the x-axis
 
     from plot_3d import plot_3D
-    # filtered_df = pd.read_csv(output_3d) #uncomment this if you have a saved df you just want to read and plot in 3d
-    plot_3D(filtered_df,sn_name)
+    # filtered_df = pd.read_csv(output_file,index_col=0,header=0) #uncomment this if you have a saved df you just want to read and plot in 3d
+    # mjd_list = np.array(filtered_df.columns.values,dtype='float')
+    # wavelengths = np.array(filtered_df.index.values,dtype='float')
+    # flux_matrix = np.array(filtered_df.values,dtype='float')
+    plot_3D(mjd_list,wavelengths,flux_matrix,sn_name)
