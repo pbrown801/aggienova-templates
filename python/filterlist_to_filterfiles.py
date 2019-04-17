840835f139971c30241b57cac2167b735690831f
Tate Walker <tatewalker@comcast.net>
2019-04-17 14:20:59 -0500

    filterlist_to_file

--- python/filterlist_to_filterfiles.py
+++ python/filterlist_to_filterfiles.py
@@ -3,43 +3,43 @@ def filterlist_to_filterfilelist(filterlist):
 
     filterfilelist=filterlist
 
-    for filter in filterfilelist:
-
-	if filter == UVW2:
-		filter='UVW2_2010.txt'
-	if filter == UVM2:
-		filter='UVM2_2010.txt'
-	if filter == UVW1:
-		filter='UVW1_2010.txt'
-	if filter == U:
-		filter='U_UVOT.txt'
-	if filter == B:
-		filter='B_UVOT.txt'
-	if filter == V:
-		filter='V_UVOT.txt'
-	if filter == g:
-		filter='LSST_g.dat'
-	if filter == r:
-		filter='LSST_r.dat'
-	if filter == i:
-		filter='LSST_i.dat'
-	if filter == u:
-		filter='LSST_u.dat'
-	if filter == z:
-		filter='LSST_z.dat'
-	if filter == y:
-		filter='LSST_y4.dat'
-	if filter == F200W:
-		filter='F200W_NRC_and_OTE_ModAB_mean.txt'
-	if filter == F444W:
-		filter='F444W_NRC_and_OTE_ModAB_mean.txt'
+    for idx,filter in enumerate(filterfilelist):
+        print(filter)
+        if filter == 'UVW2':
+            filterfilelist[idx]='UVW2_2010.txt'
+        if filter == 'UVM2':
+            filterfilelist[idx]='UVM2_2010.txt'
+        if filter == 'UVW1':
+            filterfilelist[idx]='UVW1_2010.txt'
+        if filter == 'U':
+            filterfilelist[idx]='U_UVOT.txt'
+        if filter == 'B':
+            filterfilelist[idx]='B_UVOT.txt'
+        if filter == 'V':
+            filterfilelist[idx]='V_UVOT.txt'
+        if filter == 'g':
+            filterfilelist[idx]='LSST_g.dat'
+        if filter == 'r':
+            filterfilelist[idx]='LSST_r.dat'
+        if filter == 'i':
+            filterfilelist[idx]='LSST_i.dat'
+        if filter == 'u':
+            filterfilelist[idx]='LSST_u.dat'
+        if filter == 'z':
+            filterfilelist[idx]='LSST_z.dat'
+        if filter == 'y':
+            filterfilelist[idx]='LSST_y4.dat'
+        if filter == 'F200W':
+            filterfilelist[idx]='F200W_NRC_and_OTE_ModAB_mean.txt'
+        if filter == 'F444W':
+            filterfilelist[idx]='F444W_NRC_and_OTE_ModAB_mean.txt'
 
     return filterfilelist;
 
 filterlist = ['UVW2', 'UVM2','UVW1',  'U', 'B', 'V','R', 'I']
-filterlist_to_filterfilelist(filterlist)
-print, filterfilelist
+filterfilelist = filterlist_to_filterfilelist(filterlist)
+print(filterfilelist)
 
-                    
-                                              
-                        
+
+
+
