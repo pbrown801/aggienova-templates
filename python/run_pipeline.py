a336273ac4dcb1e1d7cf4d01480f900f5621f4fe
Peter Brown <peterbrown@byu.net>
2019-09-30 11:20:05 -0500

    added sample run command to top

--- python/run_pipeline.py
+++ python/run_pipeline.py
@@ -1,3 +1,6 @@
+# this is what Peter can run and get a count rate plot and a 3d plot
+# python3 run_pipeline.py SN2007af SN2017erp_hst_20170629.dat y
+
 #  take an input spectrum
 import pandas as pd
 import numpy as np
