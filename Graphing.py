ecca88cabe7f0ddc06cc0e3776f5e51ec7210875
megamp15 <47288803+megamp15@users.noreply.github.com>
2019-10-06 02:12:05 -0500

    Main graphing function started

--- Graphing.py
+++ Graphing.py
@@ -5,21 +5,21 @@
 
 import numpy
 from matplotlib.pylab import *
-from mpl_toolkits.axes_grid1 import host_subplot
 import matplotlib.animation as animation
-
+import matplotlib.pyplot as plt
+import csv
 # Sent for figure
 font = {'size': 9}
 matplotlib.rc('font', **font)
 
 # Setup figure and subplots
-f0 = figure(num=0, figsize=(12, 8))  # , dpi = 100)
-f0.suptitle("Plot Traversal Animation", fontsize=12)
+f0=plt.figure(num=0, figsize=(8, 8))
+f0.suptitle("Plot Traversal Animation", fontsize=10)
 ax01 = subplot2grid((2, 2), (0, 0))
 ax02 = subplot2grid((2, 2), (0, 1))
-ax03 = subplot2grid((2, 2), (1, 0), colspan=2, rowspan=1)
-ax04 = ax03.twinx()
-# tight_layout()
+ax03 = subplot2grid((2, 2), (1, 0))
+ax04 = subplot2grid((2, 2), (1, 1))
+f0.subplots_adjust(hspace=1.5)
 
 # Set titles of subplots
 ax01.set_title('Light Curve')
@@ -61,18 +61,19 @@ t = zeros(0)
 
 # set plots
 p011, = ax01.plot(t, yp1, 'b-', label="yp1")
-p012, = ax01.plot(t, yp2, 'g-', label="yp2")
+# p012, = ax01.plot(t, yp2, 'g-', label="yp2")
 
 p021, = ax02.plot(t, yv1, 'b-', label="yv1")
-p022, = ax02.plot(t, yv2, 'g-', label="yv2")
+# p022, = ax02.plot(t, yv2, 'g-', label="yv2")
 
 p031, = ax03.plot(t, yp1, 'b-', label="yp1")
-p032, = ax04.plot(t, yv1, 'g-', label="yv1")
+# p032, = ax04.plot(t, yv1, 'g-', label="yv1")
 
-# set lagends
-ax01.legend([p011, p012], [p011.get_label(), p012.get_label()])
-ax02.legend([p021, p022], [p021.get_label(), p022.get_label()])
-ax03.legend([p031, p032], [p031.get_label(), p032.get_label()])
+# # set lagends
+# ax01.legend([p011, p012], [p011.get_label(), p012.get_label()])
+# ax02.legend([p021, p022], [p021.get_label(), p022.get_label()])
+# ax03.legend([p031, p032], [p031.get_label(), p032.get_label()])
+# ax03.legend([p031, p032], [p031.get_label(), p032.get_label()])
 
 # Data Update
 # This will become epoch. Need to make sure the data is based on epoch.
@@ -92,35 +93,37 @@ def updateData(self):
 
     tmpp1 = 1 + exp(-x) * sin(2 * pi * x)
     tmpv1 = - exp(-x) * sin(2 * pi * x) + exp(-x) * cos(2 * pi * x) * 2 * pi
-    yp1 = append(yp1, tmpp1)
+    yp1=append(yp1,tmpp1)
     yv1 = append(yv1, tmpv1)
     yp2 = append(yp2, 0.5 * tmpp1)
     yv2 = append(yv2, 0.5 * tmpv1)
     t = append(t, x)
-
-    x += 0.05
+    x += 0.1
 
     p011.set_data(t, yp1)
-    p012.set_data(t, yp2)
+    # p012.set_data(t, yp2)
 
     p021.set_data(t, yv1)
-    p022.set_data(t, yv2)
+    # p022.set_data(t, yv2)
 
     p031.set_data(t, yp1)
-    p032.set_data(t, yv1)
+    # p032.set_data(t, yv1)
 
     if x >= xmax - 1.00:
         p011.axes.set_xlim(x - xmax + 1.0, x + 1.0)
         p021.axes.set_xlim(x - xmax + 1.0, x + 1.0)
         p031.axes.set_xlim(x - xmax + 1.0, x + 1.0)
-        p032.axes.set_xlim(x - xmax + 1.0, x + 1.0)
+        # p032.axes.set_xlim(x - xmax + 1.0, x + 1.0)
+        print("x",x)
+        print("x2",x - xmax + 1.0)
+        print("x3",x+1.0)
+    return p011, p021, p031,
 
-    return p011, p012, p021, p022, p031, p032
 
 
 # interval: draw new frame every 'interval' ms
 # frames: number of frames to draw
-simulation = animation.FuncAnimation(f0, updateData, blit=False, frames=200, interval=20, repeat=False)
+simulation = animation.FuncAnimation(f0, updateData, blit=False, frames=100, interval=10, repeat=False)
 
 # Uncomment the next line if you want to save the animation
 # simulation.save(filename='sim.mp4',fps=30,dpi=300)
