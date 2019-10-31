2da88d97a236e4e708721b10ba889b4b1c916625
megamp15 <47288803+megamp15@users.noreply.github.com>
2019-10-30 20:27:51 -0500

    Graphing_2 is the automation program of graphing. Plotting test 2 is another test file.

--- plotting_test_2.py
+++ plotting_test_2.py
@@ -3,30 +3,68 @@ import matplotlib.pyplot as plt
 import scipy.interpolate as interp
 import pandas as pd
 from matplotlib.animation import FuncAnimation
+import csv
+
+mag_data=[]
+time = []
+f=[]
+avg=[]
+p=[]
+t=[]
+p01=[]
+x=[]
+
+file = open("output\SN2007af_magarray.csv", 'r', newline='').readlines()
+reader = csv.reader(file, delimiter=',')
+filters_from_csv = next(reader)[1::2]
+
+for filters in filters_from_csv:
+    data = pd.read_csv("output\SN2007af_magarray.csv", sep=",")
+    data = data.dropna(axis=0, how='all')
+    data = data.set_index('Time (MJD)')
+    time_temp=list(data.index.values)
+    time_1=time_temp[0]
+    time_2=time_temp[len(time_temp)-1]
+    data=data[filters]
+    data=data.dropna()
+    mag_data.append(list(data))
+    time.append(list(data.index.values))
+for i in range(len(mag_data)):
+    tot = 0
+    for j in range(len(mag_data[i])):
+        tot+=mag_data[i][j]
+    avg.append(tot/len(mag_data[i]))
+for i in range(len(filters_from_csv)):
+    if i!=4:
+        time[i] = [time_1] + time[i] + [time_2]
+        mag_data[i]=[avg[i]]+mag_data[i]+[avg[i]]
+    else:
+        time[i] = time[i] + [time_2]
+        mag_data[i]=mag_data[i]+[avg[i]]
 
 plt.style.use('dark_background')
 
-time_UVW2 = [54161.8408, 54165.0591, 54177.5535, 54200.1088, 54213.7318, 54309.2917]
-UVW2 = [((18.788 + 18.815 + 17.499 + 17.358) / 4), 18.788, 18.815, 17.499, 17.358,((18.788 + 18.815 + 17.499 + 17.358) / 4)]
-time_UVM2 = [54161.8408, 54165.0591, 54177.5535, 54200.1088, 54213.7318, 54309.2917]
-UVM2 = [(0.189 + 0.195 + 0.121 + 0.115) / 4, 0.189, 0.195, 0.121, 0.115, (0.189 + 0.195 + 0.121 + 0.115) / 4]
-time_UVW1 = [54161.8408, 54170.6312, 54179.5612, 54204.4517, 54217.2887, 54309.2917]
-UVW1 = [(19.673 + 19.636 + 18.966 + 19.253) / 4, 19.673, 19.636, 18.966, 19.253,(19.673 + 19.636 + 18.966 + 19.253) / 4]
-time_U = [54161.8408,54204.4517,54309.2917]
-U = [0.314,0.314,0.314]
-time_B = [54161.8408, 54171.5313, 54206.5268, 54309.2917]
-B = [((17.243+15.638)/2), 17.243, 15.638, ((17.243+15.638)/2)]
-time_V = [54161.8408, 54171.5313, 54206.5268, 54309.2917]
-V = [((0.088+0.07)/2), 0.088, 0.07, ((0.088+0.07)/2)]
-time_R = [54161.8408, 54172.3682, 54208.7714, 54237.9345, 54272.4349, 54309.2917]
-R = [((15.38 + 13.885 + 14.67 + 14.432) / 4), 15.38, 13.885, 14.67, 14.432, ((15.38 + 13.885 + 14.67 + 14.432) / 4)]
-time_I = [54161.8408, 54172.3682, 54208.7714, 54237.9345, 54272.4349, 54309.2917]
-I = [((0.057 + 0.043 + 0.008 + 0.009) / 4), 0.057, 0.043, 0.008, 0.009, ((0.057 + 0.043 + 0.008 + 0.009) / 4)]
+time_UVW2 = time[0]
+UVW2 = mag_data[0]
+time_UVM2 = time[1]
+UVM2 = mag_data[1]
+time_UVW1 = time[2]
+UVW1 = mag_data[2]
+time_U = time[3]
+U = mag_data[3]
+time_B = time[4]
+B = mag_data[4]
+time_V = time[5]
+V = mag_data[5]
+time_R = time[6]
+R = mag_data[6]
+time_I = time[7]
+I = mag_data[7]
 
 f1 = interp.interp1d(time_UVW2, UVW2, kind='cubic')
 f2 = interp.interp1d(time_UVM2, UVM2, kind='cubic')
 f3 = interp.interp1d(time_UVW1, UVW1, kind='cubic')
-f4 = interp.interp1d(time_U, U, kind='nearest')
+f4 = interp.interp1d(time_U, U, kind='cubic')
 f5 = interp.interp1d(time_B, B, kind='cubic')
 f6 = interp.interp1d(time_V, V, kind='cubic')
 f7 = interp.interp1d(time_R, R, kind='cubic')
@@ -34,26 +72,14 @@ f8 = interp.interp1d(time_I, I, kind='cubic')
 
 f0 = plt.figure(num=0, figsize=(8, 8))
 ax01 = f0.add_subplot(211)
-ax02 = f0.add_subplot(212)
-# ax03 = f0.add_subplot(222)
-# ax01 = subplot2grid((2, 2), (0, 0))
-# ax02 = subplot2grid((2, 2), (0, 1))
-# ax03 = subplot2grid((2, 2), (1, 0))
-# ax04 = subplot2grid((2, 2), (1, 1))
 f0.subplots_adjust(hspace=1.5)
 f0.patch.set_facecolor('xkcd:black')
-ax01.set_title('MagArray plot versus Time (mjd)')
-
-ax01.set_ylim(0, 25)
-ax02.set_ylim(0, 0.35)
-
-# ax01.grid(True)
-# ax02.grid(True)
+ax01.set_title('MagArray plot versus Time (MJD)')
+ax01.set_ylim(8, 23)
 plt.tight_layout()
 
-ax02.set_xlabel("Time (mjd)")
 ax01.set_ylabel("MagArray of different Wavelengths")
-ax02.set_ylabel("MagArray of different Wavelengths")
+ax01.set_xlabel("Time (MJD)")
 p1 = zeros(0)
 p2 = zeros(0)
 p3 = zeros(0)
@@ -72,17 +98,17 @@ t6 = zeros(0)
 t7 = zeros(0)
 t8 = zeros(0)
 
-p01, = ax01.plot(time_UVW2, UVW2, 'b-', label='UVW2')
-p02, = ax02.plot(time_UVM2, UVM2, 'green', label='UVM2')
+p01, = ax01.plot(time_UVW2, UVW2, 'w-', label='UVW2')
+p02, = ax01.plot(time_UVM2, UVM2, 'green', label='UVM2')
 p03, = ax01.plot(time_UVW1, UVW1, 'r-', label='UVW1')
-p04, = ax02.plot(time_U, U, 'c-', label='U')
+p04, = ax01.plot(time_U, U, 'c-', label='U')
 p05, = ax01.plot(time_B, B, 'm-', label='B')
-p06, = ax02.plot(time_V, V, 'y-', label='V')
+p06, = ax01.plot(time_V, V, 'y-', label='V')
 p07, = ax01.plot(time_R, R, 'gray', label='R')
-p08, = ax02.plot(time_I, I, 'w-', label='I')
+p08, = ax01.plot(time_I, I, 'w-', label='I')
 
-ax01.legend([p01, p03, p05, p07], [p01.get_label(), p03.get_label(), p05.get_label(), p07.get_label()])
-ax02.legend([p02, p04, p06, p08], [p02.get_label(), p04.get_label(), p06.get_label(), p08.get_label()])
+ax01.legend([p01, p02, p03, p04, p05, p06, p07, p08], [p01.get_label(), p02.get_label(), p03.get_label(), p04.get_label(), p05.get_label(), p06.get_label(), p07.get_label(), p08.get_label()])
+
 x1 = time_UVW2[0]
 x2 = time_UVM2[0]
 x3 = time_UVW1[0]
@@ -92,12 +118,12 @@ x6 = time_V[0]
 x7 = time_R[0]
 x8 = time_I[0]
 
-
 def animate(self):
     global x1, x2, x3, x4, x5, x6, x7, x8
     global p1, p2, p3, p4, p5, p6, p7, p8
     global t1, t2, t3, t4, t5, t6, t7, t8
     global time_UVW2, time_UVM2, time_UVW1, time_U, time_B, time_V, time_R, time_I
+
     tp1 = f1(x1)
     tp2 = f2(x2)
     tp3 = f3(x3)
@@ -125,14 +151,14 @@ def animate(self):
     t7 = append(t7, x7)
     t8 = append(t8, x8)
 
-    inte1 = (time_UVW2[5] - time_UVW2[0]) / 1000
-    inte2 = (time_UVM2[5] - time_UVM2[0]) / 1000
-    inte3 = (time_UVW1[5] - time_UVW1[0]) / 1000
-    inte4 = (time_U[2] - time_U[0]) / 1000
-    inte5 = (time_B[3] - time_B[0]) / 1000
-    inte6 = (time_V[3] - time_V[0]) / 1000
-    inte7 = (time_R[5] - time_R[0]) / 1000
-    inte8 = (time_I[5] - time_I[0]) / 1000
+    inte1 = (time_UVW2[len(time_UVW2)-1] - time_UVW2[0]) / 1000
+    inte2 = (time_UVM2[len(time_UVM2)-1] - time_UVM2[0]) / 1000
+    inte3 = (time_UVW1[len(time_UVW1)-1] - time_UVW1[0]) / 1000
+    inte4 = (time_U[len(U)-1] - time_U[0]) / 1000
+    inte5 = (time_B[len(B)-1] - time_B[0]) / 1000
+    inte6 = (time_V[len(V)-1] - time_V[0]) / 1000
+    inte7 = (time_R[len(R)-1] - time_R[0]) / 1000
+    inte8 = (time_I[len(I)-1] - time_I[0]) / 1000
 
     x1 += inte1
     x2 += inte2
@@ -151,7 +177,8 @@ def animate(self):
     p06.set_data(t6, p6)
     p07.set_data(t7, p7)
     p08.set_data(t8, p8)
-
+    print(p01)
+    print(p02)
     return p01, p02, p03, p04, p05, p06, p07, p08
 
 
