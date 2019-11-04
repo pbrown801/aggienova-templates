f5561c0eeb7248f44848ffe7930e1862f299a74f
jenniferamartin21 <48300490+jenniferamartin21@users.noreply.github.com>
2019-11-03 21:04:59 -0600

    updated filterlist_to_filterfiles.py

--- python/filterlist_to_filterfiles.py
+++ python/filterlist_to_filterfiles.py
@@ -3,21 +3,21 @@ def filterlist_to_filterfiles(filterlist):
     filterfilelist=[' '] * len(filterlist)
     for idx,filter in enumerate(filterlist):
         if filter == 'UVW2' or filter == 'UVM2' or filter == 'UVW1':
-            filterfilelist[idx]='../filters/%s_2010.txt' % filter
+            filterfilelist[idx]='../filters/{}_2010.txt'.format(filter) #inserts filter in the curly braces
         if filter == 'U' or filter == 'B' or filter == 'V':
-            filterfilelist[idx]='../filters/%s_UVOT.txt' % filter
+            filterfilelist[idx]='../filters/{}_UVOT.txt'.format(filter)
         if filter == 'R':
             filterfilelist[idx]='../filters/R_Harris_c6004.txt'
         if filter == 'I':
             filterfilelist[idx]='../filters/johnson_i.txt'
         if filter == 'g' or filter == 'r' or filter == 'i' or filter == 'u' or filter =='z':
-            filterfilelist[idx]='../filters/LSST_%s.dat' % filter
+            filterfilelist[idx]='../filters/LSST_{}.dat'.format(filter)
         if filter == 'y':
             filterfilelist[idx]='../filters/LSST_y4.dat'
         if filter == 'F200W' or filter == 'F444W':
-            filterfilelist[idx]='../filters/%s_NRC_and_OTE_ModAB_mean.txt' % filter
+            filterfilelist[idx]='../filters/{}_NRC_and_OTE_ModAB_mean.txt'.format(filter)
         if filter == 'J' or filter == 'H':
-            filterfilelist[idx]='../filters/%s_2mass.txt' % filter
+            filterfilelist[idx]='../filters/{}_2mass.txt'.format(filter)
         if filter == 'K':
             filterfilelist[idx]='../filters/Ks_2mass.txt'
 
