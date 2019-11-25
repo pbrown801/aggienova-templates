a67a815caffb84f5df22c5c892efeee618652340
Drager Landry <dr4g3r@tamu.edu>
2019-11-25 14:20:02 -0600

    Validation Plotting

--- python/validation_plotting.py
+++ python/validation_plotting.py
@@ -7,20 +7,23 @@ import csv
 # applying the file to be passed through the entire code
 def validation_plotting(filters_curve,count_rate,mjd,mangled_counts, sn_name):
     # pull the count rate, mangled counts, and mjd for u filters_curve
-    pd.read_csv('./input/'+str(sn_name)+'_countsarray', skiprows=1)
-    pd.read_csv('./input/'+str(sn_name)+'_mangledcounts', skiprows=1)
+    pd.read_csv('../input/'+str(sn_name)+'_countsarray.csv', skiprows=1)
+    pd.read_csv('../input/'+str(sn_name)+'_mangledcounts.csv', skiprows=1)
     for i in range(len(filters_curve)):
         cur_count = count_rate[:,i]
         mng_count = mangled_counts[:,i]
         x = mjd
         y = cur_count
         y2 = mng_count
-        # plt.figure(i)
+        plt.figure()
         plt.plot(x, y, color='blue', linewidth=2)
         plt.plot(x, y2, color='red', linewidth=2)
         plt.title('Comparison Plot')
         plt.xlabel('Time (mjd)')
         plt.ylabel('Count rate')
-    legend_items = list(map('{0} band'.format,filters_curve))
-    plt.legend(legend_items)
-    plt.show()
+        legend_items = ("Observed" +filters_curve[i], "Output "+filters_curve[i])
+        plt.legend(legend_items)
+        plt.show()
+        plt.savefig("../output/"+sn_name+filters_curve[i]+".png")
+        plt.clf()
+     
